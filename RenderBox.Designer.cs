b6d132bee7c0d6a58bea3ae021462ea0947a3bd4
Tomas Bosek <bosektom@gmail.com>
2016-01-31 18:58:20 +0100

    Added simple RenderBox.

---
+++ RenderBox.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace AdvancedUIControls
+{
+    partial class RenderBox
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.snapPositionStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.zoomStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.Color.White;
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.SizeChanged += new System.EventHandler(this.pictureBox_SizeChanged);
+            this.pictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox_Paint);
+            this.pictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            this.pictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseMove);
+            this.pictureBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseUp);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.snapPositionStatus,
+            this.zoomStatus});
+            this.statusStrip.Location = new System.Drawing.Point(0, 278);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(300, 22);
+            this.statusStrip.SizingGrip = false;
+            this.statusStrip.TabIndex = 1;
+            this.statusStrip.Text = "statusStrip";
+            // 
+            // snapPositionStatus
+            // 
+            this.snapPositionStatus.Name = "snapPositionStatus";
+            this.snapPositionStatus.Size = new System.Drawing.Size(0, 17);
+            // 
+            // zoomStatus
+            // 
+            this.zoomStatus.Name = "zoomStatus";
+            this.zoomStatus.Size = new System.Drawing.Size(0, 17);
+            // 
+            // RenderBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "RenderBox";
+            this.Size = new System.Drawing.Size(300, 300);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.RenderBox_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel snapPositionStatus;
+        private System.Windows.Forms.ToolStripStatusLabel zoomStatus;
+    }
+}
---
+++ RenderBox.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace AdvancedUIControls
+{
+    public enum SnapMode
+    {
+        Normal = 0,
+        Half = 1
+    }
+
+    public delegate void TranslationHandler(RenderBox control);
+    public delegate void ZoomHandler(RenderBox control, float zoom);
+    public delegate void RenderHandler(RenderBox control, Graphics graphics);
+    public partial class RenderBox : UserControl
+    {
+        #region GridCellSize
+        private readonly int gridCellWidth;
+        public int GridCellWidth { get { return gridCellWidth; } }
+
+        private readonly int gridCellHeight;
+        public int GridCellHeight { get { return gridCellHeight; } }
+        #endregion
+
+        #region Pens
+        public Pen GridPen { set; get; }
+        public Pen GridHoverPen { set; get; }
+        #endregion
+
+        #region Zoom
+        public float MinZoom { get; set; }
+        public float MaxZoom { get; set; }
+        public float DeltaZoom { get; set; }
+        private float zoom = 1.0f;
+        public float Zoom { get { return zoom; } }
+        public readonly ZoomHandler OnZoomChanged;
+        #endregion
+
+        #region Translation
+        private bool translationAllowed = true;
+        public bool TranslationAllowed { get { return translationAllowed; } }
+        private bool isTranslating;
+        private Point translatingStartPoint = Point.Empty;
+
+        public readonly TranslationHandler OnTranslationStarted;
+        public readonly TranslationHandler OnTranslationStopped;
+        #endregion
+
+        private Point renderOffset = Point.Empty;
+        private Point mousePosition = Point.Empty;
+        public readonly RenderHandler OnRender;
+
+        public RenderBox(int gridCellWidth = 32, int gridCellHeight = 32)
+        {
+            InitializeComponent();
+
+            this.gridCellWidth = gridCellWidth;
+            this.gridCellHeight = gridCellHeight;
+
+            GridPen = new Pen(Color.Black, 2.0f);
+            GridHoverPen = new Pen(Color.Red, 2.0f)
+            {
+                DashStyle = System.Drawing.Drawing2D.DashStyle.Dash
+            };
+
+            MinZoom = 0.5f;
+            MaxZoom = 1.0f;
+            DeltaZoom = 0.25f;
+            this.MouseWheel += handleZoom;
+
+            setSnapPositionStatus(Point.Empty);
+            setZoomStatus(zoom);
+        }
+
+        public void ToggleTranslation()
+        {
+            if (translationAllowed)
+                stopTranslation();
+
+            translationAllowed = !translationAllowed;
+        }
+        private void startTranslation()
+        {
+            if (!translationAllowed || isTranslating)
+                return;
+
+            isTranslating = true;
+            translatingStartPoint = ToRelativePoint(mousePosition);
+
+            OnTranslationStarted?.Invoke(this);
+        }
+        private void stopTranslation()
+        {
+            isTranslating = false;
+            translatingStartPoint = Point.Empty;
+
+            Cursor = Cursors.Default;
+
+            OnTranslationStopped?.Invoke(this);
+        }
+
+        private void setSnapPositionStatus(Point snappedMousePosition)
+        {
+            snapPositionStatus.Text = $"X: {snappedMousePosition.X} Y: {snappedMousePosition.Y}";
+        }
+        private void setZoomStatus(float zoom)
+        {
+            zoomStatus.Text = $"Z: {(int)(zoom * 100)}%";
+        }
+        private void pictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Middle)
+                return;
+            startTranslation();
+        }
+        private void pictureBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Middle)
+                return;
+            stopTranslation();
+        }
+        private void pictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            mousePosition = new Point((int)(e.X / zoom), (int)(e.Y / zoom));
+            if (isTranslating)
+                renderOffset = new Point(mousePosition.X - translatingStartPoint.X, mousePosition.Y - translatingStartPoint.Y);
+            pictureBox.Invalidate();
+        }
+        private void handleZoom(object sender, MouseEventArgs e)
+        {
+            if (e.Delta > 0 && zoom < MaxZoom)
+                zoom += DeltaZoom;
+            else if (e.Delta < 0 && zoom > MinZoom)
+                zoom -= DeltaZoom;
+
+            setZoomStatus(zoom);
+            pictureBox.Invalidate();
+
+            OnZoomChanged?.Invoke(this, zoom);
+        }
+        private void RenderBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F1)
+                ToggleTranslation();
+        }
+        private void pictureBox_SizeChanged(object sender, EventArgs e)
+        {
+            pictureBox.Invalidate();
+        }
+
+        private void drawGrid(Graphics graphics)
+        {
+            var width = pictureBox.Width;
+            var height = pictureBox.Height;
+
+            for (int x = 0; x < ((width / zoom) / GridCellWidth) + 1; x++)
+                graphics.DrawLine(GridPen,
+                    x * GridCellWidth + (renderOffset.X % GridCellWidth), 0,
+                    x * GridCellWidth + (renderOffset.X % GridCellWidth), height / zoom);
+            for (int y = 0; y < (height / zoom) / GridCellHeight; y++)
+                graphics.DrawLine(GridPen,
+                    0, y * GridCellHeight + (renderOffset.Y % GridCellHeight),
+                    width / zoom, y * GridCellHeight + (renderOffset.Y % GridCellHeight));
+        }
+        private void drawSelectionBox(Graphics graphics)
+        {
+            var grid = GetSnappedPoint(mousePosition);
+
+            setSnapPositionStatus(ToRelativePoint(grid));
+            graphics.DrawRectangle(GridHoverPen, grid.X, grid.Y, GridCellWidth, GridCellHeight);
+
+            //Debug lines
+            graphics.DrawLine(GridHoverPen, mousePosition, grid);
+            graphics.DrawLine(GridHoverPen, mousePosition, renderOffset);
+        }
+        private void pictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            //e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.;
+            //e.Graphics.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+            e.Graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+
+            e.Graphics.ScaleTransform(zoom, zoom);
+
+            drawGrid(e.Graphics);
+            drawSelectionBox(e.Graphics);
+            OnRender?.Invoke(this, e.Graphics);
+
+            e.Graphics.TranslateTransform(renderOffset.X, renderOffset.Y);
+        }
+
+        public Point ToRelativePoint(Point point)
+        {
+            return new Point(point.X - renderOffset.X, point.Y - renderOffset.Y);
+        }
+        public Point ToAbsolutePoint(Point point)
+        {
+            return new Point(point.X + renderOffset.X, point.Y + renderOffset.Y);
+        }
+        public Point GetSnappedPoint(Point point, SnapMode snapMode = SnapMode.Normal)
+        {
+            var relativePoint = ToRelativePoint(point);
+            var relativeX = relativePoint.X;
+            var relativeY = relativePoint.Y;
+
+            var closestGridX = 0;
+            var closestGridY = 0;
+            switch (snapMode)
+            {
+                case SnapMode.Normal:
+                    closestGridX = relativeX - (relativeX % GridCellWidth) - (relativeX < 0 ? GridCellWidth : 0);
+                    closestGridY = relativeY - (relativeY % GridCellHeight) - (relativeY < 0 ? GridCellHeight : 0);
+                    break;
+                case SnapMode.Half:
+                    var halfCellWidth = GridCellWidth / 2;
+                    var halfCellHeight = GridCellHeight / 2;
+                    closestGridX = relativeX - (relativeX % halfCellWidth) - (relativeX < 0 ? halfCellWidth : 0);
+                    closestGridY = relativeY - (relativeY % halfCellHeight) - (relativeY < 0 ? halfCellHeight : 0);
+                    break;
+            }
+            return ToAbsolutePoint(new Point(closestGridX, closestGridY));
+        }
+
+        public Point GetSnappedMouse(SnapMode snapMode = SnapMode.Normal)
+        {
+            return GetSnappedPoint(mousePosition, snapMode);
+        }
+    }
+}
